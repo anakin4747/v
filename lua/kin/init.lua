2bd7440689ad99b80778d94049fa248a2e113280
Anakin Childerhose <anakin@childerhose.ca>
2024-04-20 17:09:08 -0400

    Best init.lua update ever
    Added error handeling to make sure that if a file does not load
    correctly the rest of your config will still load so you don't loose
    your features because you missed a comma

--- lua/kin/init.lua
+++ lua/kin/init.lua
@@ -1,6 +1,22 @@
-require "kin.lazy"
 
-require "kin.autocmds"
-require "kin.keymaps"
-require "kin.options"
-require "kin.telescope"
+local includes = {
+    'kin.lazy',
+    'kin.autocmds',
+    'kin.keymaps',
+    'kin.lsps',
+    'kin.options',
+    'kin.telescope',
+    'kin.treesitter',
+}
+
+-- Makes sure each file required exists and is syntatically correct
+-- Great for still having functionality if part of your config breaks
+for _, file in ipairs(includes) do
+    local success, err = pcall(require, file)
+    if success == false then
+        vim.notify(
+            'init.lua: failed to require file: ' .. file .. '\nerr: ' .. err,
+            vim.log.levels.WARN
+        )
+    end
+end
