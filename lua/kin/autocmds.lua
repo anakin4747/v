11ef29a3ee9f11116d8f51fe201e47b082beb90e
Anakin Childerhose <anakin@childerhose.ca>
2024-04-20 17:08:21 -0400

    Added autocommand to open all folds on BufRead

--- lua/kin/autocmds.lua
+++ lua/kin/autocmds.lua
@@ -1,6 +1,6 @@
 
 -- Yank on Highlight
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
         vim.highlight.on_yank()
@@ -9,3 +9,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
 })
 
+-- Auto Unfold All Folds
+local autounfold_group = vim.api.nvim_create_augroup('Unfold', {})
+vim.api.nvim_create_autocmd('BufRead', {
+    callback = function()
+        vim.cmd('norm zR')
+    end,
+    group = autounfold_group,
+    pattern = '*',
+})
