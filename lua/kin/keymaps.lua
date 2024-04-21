9447446939a54c433e180497efa13f0b8b05217d
Anakin Childerhose <anakin@childerhose.ca>
2024-04-20 22:40:37 -0400

    Completely restructured keymaps
    All global keymaps even from other plugins have their plugins defined in
    keymaps.lua now to make keeping track of them even easier. The only
    other place I believe there are keymaps are buffer local keymaps for
    lsps. Also made the way to define keymaps use less boilerplate.

--- lua/kin/keymaps.lua
+++ lua/kin/keymaps.lua
@@ -1,18 +1,53 @@
 local opts = { noremap = true, silent = true }
 
-local keymap = vim.api.nvim_set_keymap
-
 -- Leader becomes space
 vim.g.mapleader = " "
 
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+local builtin = require('telescope.builtin')
+local dap = require('dap')
+local ui_widgets = require('dap.ui.widgets')
 
--- -- Make space a nop
--- keymap('n', '<Space>', '<Nop>', opts)
--- keymap('v', '<Space>', '<Nop>', opts)
+local global_keymaps = {
 
--- Insert Mode Mappings --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
+    -- Navigate buffers
+    { "n", "<S-l>",      ":bnext<CR>",             opts },
+    { "n", "<S-h>",      ":bprevious<CR>",         opts },
+
+    -- Press jk fast to escape
+    { "i", "jk",         "<ESC>",                  opts },
+
+    -- Telescope keymaps
+    { 'n', '<leader>ff', builtin.find_files,       opts },
+    { 'n', '<leader>fg', builtin.live_grep,        opts },
+    { 'n', '<leader>fb', builtin.buffers,          opts },
+    { 'n', '<leader>fh', builtin.help_tags,        opts },
+    { 'n', '<leader>km', builtin.keymaps,          opts },
+
+    -- Diagnostics
+    { 'n', '<leader>e',  vim.diagnostic.open_float },
+    { 'n', '[d',         vim.diagnostic.goto_prev },
+    { 'n', ']d',         vim.diagnostic.goto_next },
+    { 'n', '<leader>q',  vim.diagnostic.setloclist },
+
+    -- Debugging
+    { 'n', '<leader>c',  dap.continue },
+    { 'n', '<leader>n',  dap.step_over },
+    { 'n', '<leader>si', dap.step_into },
+    { 'n', '<leader>so', dap.step_out },
+    { 'n', '<Leader>b',  dap.toggle_breakpoint },
+    { 'n', '<Leader>B',  dap.set_breakpoint },
+
+    { 'n', '<Leader>lp', function()
+        dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+    end },
+    { 'n',          '<Leader>dr', dap.repl.open },
+    { 'n',          '<Leader>dl', dap.run_last },
+    { { 'n', 'v' }, '<Leader>dh', ui_widgets.hover },
+    { { 'n', 'v' }, '<Leader>dp', ui_widgets.preview },
+    { 'n',          '<Leader>df', function() ui_widgets.centered_float(ui_widgets.frames) end },
+    { 'n',          '<Leader>ds', function() ui_widgets.centered_float(ui_widgets.scopes) end },
+}
+
+for _, keymap in ipairs(global_keymaps) do
+    vim.keymap.set(unpack(keymap))
+end
