19644b9efa07fe589e75cd9b7ba6afd1354cfa00
Anakin Childerhose <anakin@childerhose.ca>
2024-04-20 22:39:29 -0400

    Updated fake top level init.lua

--- lua/kin/init.lua
+++ lua/kin/init.lua
@@ -1,22 +1,29 @@
+-- TOP LEVEL init.lua
+
+require('kin.lazy') -- Needs to be first for bootstrapping
 
 local includes = {
-    'kin.lazy',
-    'kin.autocmds',
-    'kin.keymaps',
-    'kin.lsp',
-    'kin.options',
-    'kin.telescope',
-    'kin.treesitter',
+    'autocmds',
+    'dbg',
+    'keymaps',
+    'lsp',
+    'options',
+    'telescope',
+    'treesitter',
 }
+
+local debug = false
 
 -- Makes sure each file required exists and is syntatically correct
 -- Great for still having functionality if part of your config breaks
 for _, file in ipairs(includes) do
+    file = 'kin.' .. file
     local success, err = pcall(require, file)
     if success == false then
-        vim.notify(
-            'init.lua: failed to require file: ' .. file .. '\nerr: ' .. err,
-            vim.log.levels.WARN
-        )
+        local warning = 'init.lua: failed to require file: ' .. file
+        if debug == true then
+            warning = warning .. ' \n ERROR: ' .. err
+        end
+        vim.notify(warning, vim.log.levels.WARN)
     end
 end
