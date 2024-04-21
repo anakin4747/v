f0ec24dfa0909be22ad2b634b12f7455babc84e2
Anakin Childerhose <anakin@childerhose.ca>
2024-04-20 22:45:49 -0400

    UPdated where the keymaps are and lsp stuff i guess

--- lua/kin/lsp/init.lua
+++ lua/kin/lsp/init.lua
@@ -1,26 +1,31 @@
+-- LSP init.lua
 
 local includes = {
-    'kin.lsp.autotools',
-    'kin.lsp.awk',
-    'kin.lsp.bash',
-    'kin.lsp.cmake',
-    'kin.lsp.c',
-    'kin.lsp.docker',
-    'kin.lsp.javascript',
-    'kin.lsp.keymaps',
-    'kin.lsp.latex',
-    'kin.lsp.lua',
-    'kin.lsp.python',
+    'autotools',
+    'awk',
+    'bash',
+    'cmake',
+    'c',
+    'docker',
+    'javascript',
+    'keymaps',
+    'latex',
+    'lua',
+    'python',
 }
+
+local debug = false
 
 -- Makes sure each file required exists and is syntatically correct
 -- Great for still having functionality if part of your config breaks
 for _, file in ipairs(includes) do
+    file = 'kin.lsp.' .. file
     local success, err = pcall(require, file)
     if success == false then
-        vim.notify(
-            'lsp/init.lua: failed to require file: ' .. file .. '\nerr: ' .. err,
-            vim.log.levels.WARN
-        )
+        local warning = 'lsp/init.lua: failed to require file: ' .. file
+        if debug == true then
+            warning = warning .. ' \n ERROR: ' .. err
+        end
+        vim.notify(warning, vim.log.levels.WARN)
     end
 end
--- lua/kin/lsp/keymaps.lua
+++ lua/kin/lsp/keymaps.lua
@@ -1,16 +1,3 @@
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local global_mappings = {
-    { 'n', '<leader>e', vim.diagnostic.open_float },
-    { 'n', '[d',        vim.diagnostic.goto_prev },
-    { 'n', ']d',        vim.diagnostic.goto_next },
-    { 'n', '<leader>q', vim.diagnostic.setloclist },
-}
-
-for _, mapping in ipairs(global_mappings) do
-    vim.keymap.set(unpack(mapping))
-end
-
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -24,7 +11,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- See `:help vim.lsp.*` for documentation on any of the below functions
         local opts = { buffer = ev.buf }
 
-        local buffer_mappings = {
+        local buffer_keymaps = {
             { 'n',          'gD',         vim.lsp.buf.declaration,                                                 opts },
             { 'n',          'gd',         vim.lsp.buf.definition,                                                  opts },
             { 'n',          'gr',         vim.lsp.buf.references,                                                  opts },
@@ -36,12 +23,12 @@ vim.api.nvim_create_autocmd('LspAttach', {
             { 'n',          '<leader>wa', vim.lsp.buf.add_workspace_folder,                                        opts },
             { 'n',          '<leader>wr', vim.lsp.buf.remove_workspace_folder,                                     opts },
             { 'n',          '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts },
-            { { 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action,                                                 opts },
+            { { 'n', 'v' }, '<leader>Ca', vim.lsp.buf.code_action,                                                 opts },
             { 'n',          '<leader>f',  function() vim.lsp.buf.format { async = true } end,                      opts },
         }
 
-        for _, mapping in ipairs(buffer_mappings) do
-            vim.keymap.set(unpack(mapping))
+        for _, keymap in ipairs(buffer_keymaps) do
+            vim.keymap.set(unpack(keymap))
         end
     end,
 })
