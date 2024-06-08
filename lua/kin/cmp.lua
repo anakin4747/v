31e3124f54c99aa3a528e2af57121010c3299c50
Anakin Childerhose <anakin@childerhose.ca>
2024-06-08 14:08:31 -0400

    cmp: remove limit on buf cmp
    Remove the 5 character limit on buffer completions since I want them to
    be available immediately.

--- lua/kin/cmp.lua
+++ lua/kin/cmp.lua
@@ -50,7 +50,7 @@ cmp.setup({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
-        { name = 'buffer',  keyword_length = 5 },
+        { name = 'buffer' },
     },
     formatting = {
         fields = { 'abbr', 'menu' },
