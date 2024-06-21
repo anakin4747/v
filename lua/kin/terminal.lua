f7024a5fb84b0691bba9ac8c22c1ecb048727fda
Anakin Childerhose <anakin@childerhose.ca>
2024-06-20 22:18:42 -0400

    terminal: dynamically change statusbar in terminal
    To remove the status bar in a terminal buffer, change laststatus in the
    TermOpen event and add when leaving & entering terminal buffers.

--- lua/kin/terminal.lua
+++ lua/kin/terminal.lua
@@ -1,8 +1,28 @@
 
+local term_group = vim.api.nvim_create_augroup('TerminalOptionsGroup', { clear = true })
+
 vim.api.nvim_create_autocmd('TermOpen', {
-    group = vim.api.nvim_create_augroup('TerminalOptionsGroup', { clear = true }),
+    group = term_group,
     callback = function()
-        vim.cmd 'setlocal nonumber | setlocal norelativenumber'
-    end, pattern = '*',
+        vim.api.nvim_exec2([[
+            setlocal nonumber
+            setlocal norelativenumber
+            setlocal laststatus=0
+        ]], {})
+    end,
+    desc = 'Set terminal specific options',
 })
 
+vim.api.nvim_create_autocmd('BufEnter', {
+    group = term_group,
+    command = 'setlocal laststatus=0',
+    pattern = 'term://*',
+    desc = 'Disable status line if entering a terminal buffer',
+})
+
+vim.api.nvim_create_autocmd('BufLeave', {
+    group = term_group,
+    command = 'setlocal laststatus=2',
+    pattern = 'term://*',
+    desc = 'Enable status line if leaving a terminal buffer',
+})
