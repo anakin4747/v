edf5f440ddf36ba5d96187e2a5e9be7f862d1f21
Anakin Childerhose <anakin@childerhose.ca>
2024-07-26 00:18:46 -0400

    gf: add exclusion expression to filter out dirs
    Also added support for files with a % in them.

--- lua/kin/gf_callback.lua
+++ lua/kin/gf_callback.lua
@@ -5,19 +5,20 @@ local function file_exists(filename)
     return false
 end
 
-local function gf_callback()
+local function gf_wont_callback()
 
     -- Expand twice since the expanded <cfile> might contain unexpanded symbols like `~`
     local cfile = vim.fn.expand(vim.fn.expand("<cfile>"))
 
     if file_exists(cfile) then
-        vim.cmd("edit " .. cfile)
-        -- print("gf_callback.lua: opened `" .. cfile .. "` in cwd:`" .. cwd .. "`")
+        -- Replace % with \% since :edit will expand it
+        vim.cmd("edit " .. cfile:gsub("%%", "\\%%"))
         return
     end
 
     local cwd = ""
 
+    -- Aware of your terminal buffers' cwd
     local pid = vim.b.terminal_job_pid
     if pid ~= nil then
         local cwd_cmd = "readlink -e /proc/" .. pid .. "/cwd"
@@ -36,44 +37,84 @@ local function gf_callback()
     --]]
     if file_exists(abs_file_path) then
         vim.cmd("edit " .. abs_file_path)
-        -- print("gf_callback.lua: opened `" .. cfile .. "` in cwd:`" .. cwd .. "`")
         return
     end
 
-    -- Make here below async
-    print("gf_callback.lua: not a complete path. SEARCHING " .. cwd)
+    print("gf_wont_callback: not a complete path. SEARCHING " .. cwd)
+
+
+    -- Now use GNU find to find the files
+    --
+    -- find cwd \( -name foo -o -path bar/baz \) -prune -o -name <target-file> -print 2> /dev/null
+    --
+    -- The `\( ... \) -prune -o ... -print ...` portion is required for
+    -- excluding files. The group `\( ... \)` is an arbitrary number of -name
+    -- foo and -path bar/baz flags or'd together (-o). It is generated from the
+    -- space separated items in the following string:
+
+    vim.g.gf_excludes = "build .git"
+
+    -- The -prune is needed to exclude the -names and -paths from the above
+    -- string. The -print is required to get ride of the default feature of
+    -- -prune which is to still print the path to the pruned branches as well.
+    -- The `\( ... \) -prune -o ` portion is created by the exclude_expr().
+
+
+    -- TODO You could probably cache the string this builds and just rebuild it when the
+    --      old gf_xcld_expr doesn't match the current gf_xcld_expr to avoid rerunning
+    --      this needlessly
+    local function exclude_expr()
+
+        if vim.g.gf_excludes == "" then
+            return ""
+        end
+
+        local find_expr = " \\("
+
+        local excluded_files = vim.split(vim.g.gf_excludes, ' ')
+
+        for i, excluded_file in ipairs(excluded_files) do
+            if excluded_file:find('/') then
+                find_expr = find_expr .. " -path " .. excluded_file
+            else
+                find_expr = find_expr .. " -name " .. excluded_file
+            end
+
+            if i ~= #excluded_files then
+                find_expr = find_expr .. " -o"
+            end
+        end
+
+        return find_expr .. " \\) -prune -o "
+    end
 
     local find_cmd = ""
 
     if cfile:find('/') then
-        -- print("gf_callback.lua: slashes in <cfile>")
-        find_cmd = "find " .. cwd .. " -path '*" .. cfile .. "*' 2> /dev/null"
+        find_cmd = "find " .. cwd .. exclude_expr() .. " -path '*" .. cfile .. "*'"
     else
-        -- print("gf_callback.lua: no slashes in <cfile>")
-        find_cmd = "find " .. cwd .. " -name " .. cfile .. " 2> /dev/null"
+        find_cmd = "find " .. cwd .. exclude_expr() .. " -name " .. cfile
     end
+
+    find_cmd = find_cmd .. " -print 2> /dev/null"
+
+    print(find_cmd)
 
     local find_stdout = vim.fn.system(find_cmd)
 
-    -- print("\nSTDOUT\n")
-    -- print(find_stdout)
-    -- print("STDOUT END\n")
-
     local paths = vim.split(find_stdout, "\n")
 
-    -- print(#lines)
+    -- Exit if empty
     if #paths == 1 then
-        -- exit if empty
-        print("gf_callback.lua: no file found")
+        print("gf_wont_callback: no file found in " .. cwd)
         return
     end
 
+    -- If there was only one result
     if #paths == 2 then
         vim.cmd("edit " .. paths[1])
         return
     end
-
-    print("gf_callback.lua: more than one find")
 
     local qf_items = {}
 
@@ -84,10 +125,6 @@ local function gf_callback()
     -- get rid of last
     qf_items[#qf_items] = nil
 
-    -- print("\nqf_items:\n")
-    -- print(qf_items)
-    -- print("qf_items: END\n")
-
     vim.fn.setqflist({}, ' ', {
         title = "goto file",
         items = qf_items
@@ -96,4 +133,4 @@ local function gf_callback()
     vim.cmd('copen')
 end
 
-return gf_callback
+return gf_wont_callback
