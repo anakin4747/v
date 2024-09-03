feef81c342464426c1c46660f9a5737ab3128e28
Anakin Childerhose <anakin@childerhose.ca>
2024-09-02 22:53:25 -0400

    dbg: add attaching gdb configs
    Add one for connecting to a gdbserver
    Add one for launching a gdbserver to connect to

--- lua/kin/dbg/gdb.lua
+++ lua/kin/dbg/gdb.lua
@@ -3,7 +3,7 @@ local dap = require("dap")
 dap.adapters.gdb = {
     type = "executable",
     command = "gdb",
-    args = { "-i", "dap" }
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
 }
 
 local gdb_cfg = {
@@ -16,6 +16,35 @@ local gdb_cfg = {
         end,
         cwd = "${workspaceFolder}",
         stopAtBeginningOfMainSubprogram = false,
+    },
+    {
+        name = 'Attach to gdbserver :3333',
+        type = 'gdb',
+        request = 'attach',
+        target = 'localhost:3333',
+        program = '/tmp/temp/a.out',
+        cwd = '${workspaceFolder}'
+    },
+    {
+        name = 'Launch and Attach to gdbserver :3333',
+        type = 'gdb',
+        request = 'attach',
+        target = 'localhost:3333',
+        program = function ()
+            local program = '/tmp/temp/a.out'
+            local server = 'gdbserver'
+
+            -- kill gdbserver if already running
+            if vim.fn.system('pgrep ' .. server) ~= "" then
+                vim.fn.system('killall ' .. server)
+            end
+
+            vim.uv.spawn(server,
+                { args = { 'localhost:3333', program } }
+            )
+            return program
+        end,
+        cwd = '${workspaceFolder}'
     },
 }
 
