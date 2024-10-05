a64b567eb3ec90720b2727616400c297e1d62068
Anakin Childerhose <anakin@childerhose.ca>
2024-10-05 17:40:11 -0400

    dapui: remove most windows of dap UI

--- lua/kin/dbg/dapui.lua
+++ lua/kin/dbg/dapui.lua
@@ -8,20 +8,20 @@ dapui.setup({
         expanded = ""
     },
     layouts = {
-        {
-            elements = {
-                { id = "scopes", size = 0.25 },
-                { id = "breakpoints", size = 0.25 },
-                { id = "stacks", size = 0.25 },
-                { id = "watches", size = 0.25 },
-            },
-            position = "left",
-            size = 40,
-        },
+        -- {
+        --     elements = {
+        --         { id = "scopes", size = 0.25 },
+        --         { id = "breakpoints", size = 0.25 },
+        --         { id = "stacks", size = 0.25 },
+        --         { id = "watches", size = 0.25 },
+        --     },
+        --     position = "left",
+        --     size = 40,
+        -- },
         {
             elements = {
                 { id = "repl", size = 0.5 },
-                { id = "console", size = 0.5 }
+                -- { id = "console", size = 0.5 }
             },
             position = "bottom",
             size = 10,
