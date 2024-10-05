c3b35eed7421159864be1a5ba5faabe8fb503480
Anakin Childerhose <anakin@childerhose.ca>
2024-10-05 17:55:54 -0400

    dbg: clean up failed config attempts

--- lua/kin/dbg/gdb.lua
+++ lua/kin/dbg/gdb.lua
@@ -31,31 +31,15 @@ local gdb_cfg = {
         stopAtBeginningOfMainSubprogram = false,
     },
     {
-        name = 'Attach to gdbserver :3333',
-        type = 'gdb',
-        request = 'attach',
-        target = 'localhost:3333',
-        program = '/tmp/temp/a.out',
-        cwd = '${workspaceFolder}'
-    },
-    {
-        name = 'Launch and Attach to gdbserver :3333',
-        type = 'gdb',
-        request = 'attach',
-        target = 'localhost:3333',
-        program = function ()
-            local program = '/tmp/temp/a.out'
-            local server = 'gdbserver'
-
-            -- kill gdbserver if already running
-            if vim.fn.system('pgrep ' .. server) ~= "" then
-                vim.fn.system('killall ' .. server)
-            end
-
-            vim.uv.spawn(server,
-                { args = { 'localhost:3333', program } }
-            )
-            return program
+        name = "Select and attach to process",
+        type = "gdb",
+        request = "attach",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        pid = function()
+            local name = vim.fn.input('Executable name (filter): ')
+            return require("dap.utils").pick_process({ filter = name })
         end,
         cwd = '${workspaceFolder}'
     },
