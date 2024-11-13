ac071815c856bbea33259dafbb92e54084db188c
Anakin Childerhose <anakin@childerhose.ca>
2024-11-12 22:48:29 -0500

    lsp: mason: add declarative package install for tools

--- lua/kin/lsp/init.lua
+++ lua/kin/lsp/init.lua
@@ -48,3 +48,117 @@ for _, ls_cfg_file in ipairs(custom_config) do
         vim.notify(warning, vim.log.levels.WARN)
     end
 end
+
+require('mason-tool-installer').setup({
+
+    ensure_installed = {
+
+        'autotools-language-server',
+        'awk-language-server',
+        'bash-language-server',
+        'bash-debug-adapter',
+        'lua-language-server',
+        'shellcheck',
+        'vim-language-server',
+
+        -- 'ansible-language-server',
+        -- 'ansible-lint',
+        -- 'asm-lsp',
+        -- 'asmfmt',
+        -- 'ast-grep',
+        -- 'basedpyright',
+        -- 'clang-format',
+        'cmake-language-server',
+        -- 'cmakelang',
+        -- 'cmakelint',
+        -- 'cortex-debug',
+        -- 'css-lsp',
+        -- 'css-variables-language-server',
+        -- 'cssmodules-language-server',
+        'debugpy',
+        'delve',
+        -- 'docker-compose-language-service',
+        'dockerfile-language-server',
+        'ginko_ls',
+        -- 'gitlab-ci-ls',
+        -- 'go-debug-adapter',
+        -- 'gofumpt',
+        -- 'goimports',
+        -- 'goimports-reviser',
+        -- 'golangci-lint',
+        -- 'golangci-lint-langserver',
+        -- 'golines',
+        -- 'gomodifytags',
+        'gopls',
+        -- 'gospel',
+        -- 'gotests',
+        -- 'gotestsum',
+        -- 'grammarly-languageserver',
+        'groovy-language-server',
+        -- 'haskell-debug-adapter',
+        -- 'haskell-language-server',
+        -- 'html-lsp',
+        -- 'htmx-lsp',
+        -- 'java-debug-adapter',
+        -- 'java-language-server',
+        -- 'java-test',
+        -- 'json-lsp',
+        -- 'ltex-ls',
+        'lua-language-server',
+        -- 'markdown-oxide',
+        -- 'markdown-toc',
+        -- 'markdownlint',
+        -- 'markdownlint-cli2',
+        'mesonlsp',
+        -- 'perl-debug-adapter',
+        -- 'python-lsp-server',
+        -- 'r-languageserver',
+        -- 'rstcheck',
+        'rust-analyzer',
+        'shellcheck',
+        -- 'shellharden',
+        -- 'shfmt',
+        -- 'sqlls',
+        -- 'sqls',
+        'tree-sitter-cli',
+        'vim-language-server',
+        -- 'yaml-language-server',
+        -- 'yamlfix',
+        -- 'yamlfmt',
+        -- 'yamllint',
+
+    },
+
+    -- automatically install / update on startup. If set to false nothing
+    -- will happen on startup. You can use :MasonToolsInstall or
+    -- :MasonToolsUpdate to install tools and check for updates.
+    -- Default: true
+    run_on_start = true,
+
+    -- set a delay (in ms) before the installation starts. This is only
+    -- effective if run_on_start is set to true.
+    -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+    -- Default: 0
+    start_delay = 3000, -- 3 second delay
+
+    -- Only attempt to install if 'debounce_hours' number of hours has
+    -- elapsed since the last time Neovim was started. This stores a
+    -- timestamp in a file named stdpath('data')/mason-tool-installer-debounce.
+    -- This is only relevant when you are using 'run_on_start'. It has no
+    -- effect when running manually via ':MasonToolsInstall' etc....
+    -- Default: nil
+    debounce_hours = 5, -- at least 5 hours between attempts to install/update
+
+    -- By default all integrations are enabled. If you turn on an integration
+    -- and you have the required module(s) installed this means you can use
+    -- alternative names, supplied by the modules, for the thing that you want
+    -- to install. If you turn off the integration (by setting it to false) you
+    -- cannot use these alternative names. It also suppresses loading of those
+    -- module(s) (assuming any are installed) which is sometimes wanted when
+    -- doing lazy loading.
+    integrations = {
+        ['mason-lspconfig'] = true,
+        ['mason-null-ls'] = false,
+        ['mason-nvim-dap'] = true,
+    },
+})
